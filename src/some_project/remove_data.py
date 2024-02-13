23454ca91e5d60548094f3285f807b98ed1ae112
d33bs <dave.bunten@cuanschutz.edu>
2024-02-13 09:03:42 -0700

    updates to correct input / output

--- src/some_project/remove_data.py
+++ src/some_project/remove_data.py
@@ -3,10 +3,18 @@ Remove the demonstrational data dir and contents.
 """
 
 import pathlib
-import shutil
 
 # Get the data directory.
 data_dir = pathlib.Path("data")
 
+# list of files to individually remove
+files_to_remove = [
+    data_dir / "zen.txt",
+    data_dir / "zen.zip",
+    data_dir / "data_sub_dir" / "zen.txt",
+    data_dir / "data_sub_dir" / "zen.zip",
+]
+
 # Remove the data directory
-shutil.rmtree(data_dir, ignore_errors=True)
+for file in files_to_remove:
+    pathlib.Path(file).unlink()
